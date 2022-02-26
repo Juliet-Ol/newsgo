8f4327c884b917a50f05eaed60da0b27b652a78b
Juliet <agutu@gmail.com>
2022-02-27 01:06:39 +0300

    delete autogenerated imports

--- app/views.py
+++ app/views.py
@@ -1,35 +1,32 @@
-# from operator import index
-# from email import message
-# from turtle import title
-# from turtle import title
-# from crypt import methods
-# from flask import render_template
+
 from app import app
+from flask import request
 #from app.request import get_news
 
-from app.request import get_news
+from app.request import get_news, get_sources
 from flask import render_template
 
+
+
 @app.route('/')
+def home():
+    sources = get_sources()
+    return render_template('index.html', sources = sources)
+
+@app.route('/source')
 def index():
-    headline = get_news('cnn')
-    return render_template('index.html', headline = headline)
+    sources = get_sources()
+    headline = get_news(request.args.get('s'))
+    return render_template('index.html', headline = headline, sources = sources)
 
-# @app. route('/news/<int:news_id>')
-# def news(news_id):
-#     '''
-#     View news page function
-#     '''
 
-#     return render_template('news.html',id = news_id) 
+@app.route('/search', methods=['POST'])
+def search():
+    search_value = request.form['search']
+    headline = get_news(search_value)
+    return render_template('index.html', headline = headline, search = search_value)
 
-# @app. route('/news/<int:news_story>')
-# def news(news_story):
-#     '''
-#     View news page function
-#     '''
-
-#     return render_template('news.html',story = news_story)     
+ 
 
 # def headline():
 #     '''
