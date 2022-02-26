eda557247c99f7806bb2f318cac0fe76ce3ea966
Juliet <agutu@gmail.com>
2022-02-26 22:37:54 +0300

    create function that takes a list of dictionaries and get api keys

--- app/request.py
+++ app/request.py
@@ -1,45 +1,58 @@
-# from email.mime import base
-# from webbrowser import get
-# from turtle import title
-# from xml.sax import default_parser_list
 from app import app
 import urllib.request,json
 from .models import news
 
 News = news.News
 
-#getting api key
+#getting api keys
 api_key = app.config['NEWS_API_KEY']
+api_url_everything = app.config['NEWS_API_EVERYTHING']
+api_get_top_headlines_sources = app.config['NEWS_API_TOP_HEADLINES_SOURCES']
 
 #getting the news base url
 # base_url = app.config["NEWS_API_BASE_URL"]
 
-def get_news(category):
-    '''
-    Function that gets the json response to url request
-    '''
-    get_news_url = 'https://newsapi.org/v2/top-headlines?sources='+ category +'&apiKey=' + api_key
+#create get sources function that takes news category as argument
+def get_sources():
+    get_news_url = api_get_top_headlines_sources.format(api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
-        # print(get_news_data)
         get_news_response = json.loads(get_news_data)
-        # print(get_news_response['articles'])
-
-        # news_results = None
-
-        if get_news_response['articles']:
-            news_results_list = get_news_response['articles']
-            # news_results = process_results(news_results_list)
+        # news_results = []
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            
         
         return news_results_list
 
+
+def get_news(query):
+    '''
+    Function that gets the json response to url request
+
+    '''
+
+    get_news_url = api_url_everything.format(query, api_key)
+
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        news_results = []
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+        
+        return news_results
+
+#function that takes in a list of dictionaries
 def process_results(news_list):
     '''
     Function that process the news result and transform to a list 
     
     Args:
-        news_list: A list of dictionaries that contain news default_parser_list
+        news_results: A list of dictionaries that contain news default_parser_list
 
     Returns:  
         news_results: A list of news objects 
@@ -47,14 +60,17 @@ def process_results(news_list):
 
     news_results = []
     for news_item in news_list:
-        id = news_item.get('id')
-        title = news_item.get('headline_title')
-        story = story_item.get('story')
-        author = author_item.get('author')
+        url = news_item.get('url')
+        title = news_item.get('title')
+        urlToImage = news_item.get('urlToImage')
+        author = news_item.get('author')
+        description = news_item.get('description')
+        publishedAt = news_item.get('publishedAt')
+        content = news_item.get('content')
 
-        if author:
-            news_object = News(id,title,)
-            news_results.append(news.object)                
+        news = News(url, title, urlToImage, author, description, publishedAt, content)
+
+        news_results.append(news)                
 
     return news_results           
 
