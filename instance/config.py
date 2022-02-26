5353491ef236dd9e0ae5c45c8126f49de94bda2d
Juliet <agutu@gmail.com>
2022-02-26 20:43:09 +0300

    add our news API to be able do download various news sources

--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,4 @@
+NEWS_API_TOP_HEADLINES_SOURCES = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+NEWS_API_TOP_HEADLINES_SOURCE = 'https://newsapi.org/v2/top-headlines/sources={}?apiKey={}'
+NEWS_API_EVERYTHING = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
 NEWS_API_KEY = 'e4886eff42244ffaabebbfb0a9bbd892'
